15b6ae896108954b7006166b2b46d85304161b1c
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2023-01-07 12:14:52 -0600

    Setup

--- NewHorizonsTemplate/GaleGiant.cs
+++ NewHorizonsTemplate/GaleGiant.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 namespace NewHorizonsTemplate
 {
-    public class NewHorizonsTemplate : ModBehaviour
+    public class GaleGiant : ModBehaviour
     {
         public INewHorizons NewHorizonsAPI;
 
